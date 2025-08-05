e706e5f8d716d0633323ff88eacb23251336e46c
sebastianprat-commits <sebastian.prat@gmail.com>
2025-08-05 13:30:45 +0200

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBLzxWyEvisDbhb4XOech2aPYmdUbSc7fY",
   authDomain: "carterasai-831f4.firebaseapp.com",
   projectId: "carterasai-831f4",
-  storageBucket: "carterasai-831f4.firebasestorage.app",
+  storageBucket: "carterasai-831f4.appspot.com",
   messagingSenderId: "980259778594",
   appId: "1:980259778594:web:8acfd5a13e17c8a4399a7b"
 };
