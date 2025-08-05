d881a826f85017691848168677d6babd725959ba
sebastianprat-commits <sebastian.prat@gmail.com>
2025-08-05 18:52:59 +0200

    Update AuthContext.jsx

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -3,9 +3,22 @@ import { createContext, useContext, useState } from 'react'
 const AuthContext = createContext()
 
 export const AuthProvider = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    localStorage.getItem('isAuthenticated') === 'true'
+  )
+
+  const login = () => {
+    setIsAuthenticated(true)
+    localStorage.setItem('isAuthenticated', 'true')
+  }
+
+  const logout = () => {
+    setIsAuthenticated(false)
+    localStorage.removeItem('isAuthenticated')
+  }
+
   return (
-    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
       {children}
     </AuthContext.Provider>
   )
