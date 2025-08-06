496e6fb84965c02a10d0821a1497d3b5de093a44
sebastianprat-commits <sebastian.prat@gmail.com>
2025-08-07 01:13:44 +0200

    Update Cuestionario.jsx

--- src/components/Cuestionario.jsx
+++ src/components/Cuestionario.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { db } from '../firebaseConfig'
 import { collection, addDoc, Timestamp } from 'firebase/firestore'
 import { useNavigate } from 'react-router-dom'
